30eddad18bdf6493abf205e532eeb6827a572e6d
MrM <MMMohebi@outlook.com>
2022-05-31 16:51:52 +0430

    empty background

--- src/background.js
+++ src/background.js
@@ -1,9 +1,1 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.get("time",function ({time}) {
-        if(time.hasOwnProperty("hour")){
-            console.log(time);
-        }
-    })
-});
+chrome.runtime.onInstalled.addListener(() => {});
