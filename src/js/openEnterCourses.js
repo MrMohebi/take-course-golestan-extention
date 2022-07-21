8585873420178435b190252d20f4273691db4fd3
MrM <MMMohebi@outlook.com>
2022-07-21 13:22:30 +0430

    fix some bugs. not finished

--- src/js/openEnterCourses.js
+++ src/js/openEnterCourses.js
@@ -31,7 +31,7 @@ async function openEnterCourses(courseCodes){
         while (
             !document.getElementById(iframeId).contentWindow.hasOwnProperty(5) ||
             !document.getElementById(iframeId).contentWindow[4].hasOwnProperty('ReturnMenu')||
-            !document.getElementById(iframeId).contentWindow[5].document.getElementById('errtxt')?.title
+            !document.getElementById(iframeId).contentWindow[5]?.document.getElementById('errtxt')?.title.length > 1
             ){
             await new Promise(r => setTimeout(r, 500));
         }
@@ -51,21 +51,50 @@ async function openEnterCourses(courseCodes){
             console.log('page is not loaded')
         }
 
-        function AddRowT01(e,l){T01=document.getElementById(l).contentWindow[3][1].T01,hb=document.getElementById(l).contentWindow[3][1].hb;var t=[...e].reverse();window.ActiveXObject,T01.DataRow=$(T01).attr("datarow"),T01.CurrAdded=parseInt(T01.CurrAdded)+1;var n=T01.insertRow();n.className="Inserted",(s=n.insertCell()).innerHTML='<SELECT style="WIDTH: 50px;height:15px" onchange="return update_T01XML(this,\'F3125\')"><OPTION></OPTION><OPTION value="1" cl_ass="ins">ثبت</OPTION>\t<OPTION value="3" cla_ss="wai">انتظار</OPTION></SELECT>',s.childNodes[0].value=1,s=n.insertCell();var s=n.insertCell();ZCell=T01.rows[T01.DataRow-1].cells[2],s.innerHTML=ZCell.innerHTML,s.childNodes[0].value=t[0],$("input",s).removeClass("HideElement");for(var r=0;r<3;r++)s=n.insertCell(),ZCell=T01.rows[T01.DataRow-1].cells[s.cellIndex],s.innerHTML=ZCell.innerHTML,$("input",s).removeClass("HideElement"),s.childNodes[0].value=t[r+1];return s=n.insertCell(),s=n.insertCell(),s=n.insertCell(),(s=n.insertCell()).className="zw",window.ActiveXObject||(s.className="zwMB"),(s=n.insertCell()).className="zw",window.ActiveXObject||(s.className="zwMB"),s=n.insertCell(),s=n.insertCell(),s=n.insertCell(),n.scrollIntoView(),window.ActiveXObject||($(".txt",n).each(function(e,l){$(l).MBPrepare()}),2==hb&&$("td.zwMB",n).removeClass("zwMB")),n}
-
-        for (const courseCode of courseCodes) {
-            AddRowT01(courseCode, iframeId)
+        while(document.getElementById(iframeId).contentWindow[5].document.getElementById('errtxt')?.title !== "عمليات با موفقيت انجام شد."){
+            await addRowAndSubmitByFunc(iframeId, courseCodes)
+            console.log("xxxxxx")
         }
-
-        await new Promise(r => setTimeout(r, 2000));
-
-        document.getElementById(iframeId).contentWindow[4].IM13_Do_onclick()
 
         console.log('done')
     }catch (e) {
         isStarted = false
         console.log('not start')
     }
+
+}
+
+// async function addRowAndSubmitByBtn(iframeId, courseCodes){
+//     await new Promise(r => setTimeout(r, 3000));
+//
+//     document.getElementById(iframeId).contentWindow[3][1].document.querySelector('[title="اضافه کردن يک درس به مجموعه دروس در دست ثبت نام"]').click()
+//
+//     const rows = document.getElementById(iframeId).contentWindow[3][1].document.getElementsByClassName("Inserted")
+//
+//     let steps = 0
+//     for (const courseCode of courseCodes) {
+//         const courseR = [...courseCode].reverse();
+//         const row = rows[steps]
+//         for (let i = 0; i < 5 ; i++) {
+//             row.childNodes[i+2].childNodes[0].value = courseR[i]
+//         }
+//     }
+// }
+
+
+async function addRowAndSubmitByFunc(iframeId, courseCodes){
+    function AddRowT01(e,l){T01=document.getElementById(l).contentWindow[3][1].T01,hb=document.getElementById(l).contentWindow[3][1].hb;var t=[...e].reverse();window.ActiveXObject,T01.DataRow=$(T01).attr("datarow"),T01.CurrAdded=parseInt(T01.CurrAdded)+1;var n=T01.insertRow();n.className="Inserted",(s=n.insertCell()).innerHTML='<SELECT style="WIDTH: 50px;height:15px" onchange="return update_T01XML(this,\'F3125\')"><OPTION></OPTION><OPTION value="1" cl_ass="ins">ثبت</OPTION>\t<OPTION value="3" cla_ss="wai">انتظار</OPTION></SELECT>',s.childNodes[0].value=1,s=n.insertCell();var s=n.insertCell();ZCell=T01.rows[T01.DataRow-1].cells[2],s.innerHTML=ZCell.innerHTML,s.childNodes[0].value=t[0],$("input",s).removeClass("HideElement");for(var r=0;r<3;r++)s=n.insertCell(),ZCell=T01.rows[T01.DataRow-1].cells[s.cellIndex],s.innerHTML=ZCell.innerHTML,$("input",s).removeClass("HideElement"),s.childNodes[0].value=t[r+1];return s=n.insertCell(),s=n.insertCell(),s=n.insertCell(),(s=n.insertCell()).className="zw",window.ActiveXObject||(s.className="zwMB"),(s=n.insertCell()).className="zw",window.ActiveXObject||(s.className="zwMB"),s=n.insertCell(),s=n.insertCell(),s=n.insertCell(),n.scrollIntoView(),window.ActiveXObject||($(".txt",n).each(function(e,l){$(l).MBPrepare()}),2==hb&&$("td.zwMB",n).removeClass("zwMB")),n}
+
+    await new Promise(r => setTimeout(r, 3000));
+
+
+    for (const courseCode of courseCodes) {
+        AddRowT01(courseCode, iframeId)
+    }
+
+    await new Promise(r => setTimeout(r, 3000));
+
+    document.getElementById(iframeId).contentWindow[4].IM13_Do_onclick()
 
 }
 
---
+++ t1.js
@@ -0,0 +1,80 @@
+function AddRowT01(courseCode, frameID) {
+    T01 = document.getElementById(frameID).contentWindow[3][1].T01, hb = document.getElementById(frameID).contentWindow[3][1].hb;
+    var corserR = [...courseCode].reverse();
+    window.ActiveXObject, T01.DataRow = $(T01).attr("datarow"), T01.CurrAdded = parseInt(T01.CurrAdded) + 1;
+    var n = T01.insertRow();
+    n.className = "Inserted", (s = n.insertCell()).innerHTML = '<SELECT style="WIDTH: 50px;height:15px" onchange="return update_T01XML(this,\'F3125\')"><OPTION></OPTION><OPTION value="1" cl_ass="ins">ثبت</OPTION>\t<OPTION value="3" cla_ss="wai">انتظار</OPTION></SELECT>', s.childNodes[0].value = 1, s = n.insertCell();
+    var s = n.insertCell();
+    ZCell = T01.rows[T01.DataRow - 1].cells[2], s.innerHTML = ZCell.innerHTML, s.childNodes[0].value = corserR[0], $("input", s).removeClass("HideElement");
+    for (var number = 0; number < 3; number++) s = n.insertCell(), ZCell = T01.rows[T01.DataRow - 1].cells[s.cellIndex], s.innerHTML = ZCell.innerHTML, $("input", s).removeClass("HideElement"), s.childNodes[0].value = corserR[number + 1];
+    return s = n.insertCell(), s = n.insertCell(), s = n.insertCell(), (s = n.insertCell()).className = "zw", window.ActiveXObject || (s.className = "zwMB"), (s = n.insertCell()).className = "zw", window.ActiveXObject || (s.className = "zwMB"), s = n.insertCell(), s = n.insertCell(), s = n.insertCell(), n.scrollIntoView(), window.ActiveXObject || ($(".txt", n).each(function(e, l) {
+        $(l).MBPrepare()
+    }), 2 == hb && $("td.zwMB", n).removeClass("zwMB")), n
+}
+
+function AddRowT01(courseCode, frameID){
+    T01 = document.getElementById(frameID).contentWindow[3][1].T01, hb = document.getElementById(frameID).contentWindow[3][1].hb;
+    var corserR = [...courseCode].reverse();
+    if(!window.ActiveXObject)
+        T01.DataRow = $(T01).attr("datarow");
+    T01.CurrAdded=parseInt(T01.CurrAdded)+1;
+    var TR=T01.insertRow();
+    TR.className="Inserted";
+    var TD=TR.insertCell();
+    TD.innerHTML='<SELECT style="WIDTH: 50px;height:15px" onchange="return update_T01XML(this,\'F3125\')"><OPTION></OPTION><OPTION value="1" cl_ass="ins">ثبت</OPTION>	<OPTION value="3" cla_ss="wai">انتظار</OPTION></SELECT>';
+    TD.childNodes[0].value=1;
+
+    TD=TR.insertCell();
+
+//-- Course Group
+    var TD=TR.insertCell();
+    ZCell=T01.rows[T01.DataRow-1].cells[2];//Zero Row Cell Number 1
+    TD.innerHTML=ZCell.innerHTML;
+    //if(!window.ActiveXObject)
+    TD.childNodes[0].value = corserR[0]
+    $("input",TD).removeClass("HideElement")
+//-- 3 Cell For CourseNo
+    for(var k=0;k<3;k++){
+        TD=TR.insertCell();
+        ZCell=T01.rows[T01.DataRow-1].cells[TD.cellIndex];
+        TD.innerHTML=ZCell.innerHTML;
+        //if(!window.ActiveXObject)
+
+        $("input",TD).removeClass("HideElement")
+        TD.childNodes[0].value = corserR[k+1]
+    }
+//branch
+    TD = TR.insertCell();
+//-- Course Name
+    TD=TR.insertCell();
+//-- Unit
+    TD=TR.insertCell();
+// -- Lab Unit
+    TD=TR.insertCell();
+    TD.className="zw";
+    if(!window.ActiveXObject)
+        TD.className="zwMB";
+//-- Course Type
+    TD=TR.insertCell();
+    //if(!window.ActiveXObject)
+    TD.className="zw";
+    if(!window.ActiveXObject)
+        TD.className="zwMB";
+//-- Reg Status
+    TD=TR.insertCell();
+//-- Register Step
+    TD=TR.insertCell();
+//-- Notes
+    TD=TR.insertCell();
+    TR.scrollIntoView();
+    if(!window.ActiveXObject){
+        $(".txt", TR).each(function(i, v) {
+            $(v).MBPrepare();
+        })
+        if(hb==2)
+            $("td.zwMB",TR).removeClass("zwMB");
+    }
+    return TR;
+}
+
+
